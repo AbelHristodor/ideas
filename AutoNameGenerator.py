78e0022321a1d8254217fc66771818dda467a30a
AbelHristodor <habelor@gmail.com>
2018-04-17 15:49:57 +0200

    Project Finished!

--- AutoNameGenerator.py
+++ AutoNameGenerator.py
@@ -1,26 +1,38 @@
 import random, string
 
-number_input_letters = input ("How many letters do you want the name to have?  ")
+number_input_letters = input("How many letters do you want the name to have? ")
 
 input_letters = []
-for j in input_letters:
-    input_letters = input("v , c or l? ")
+
+output_letters = []
 
 vowels = "aeiou"
 consonants = "bcdfghjklmnpqrstvwxyz"
 
-for i in range(int(number_input_letters)):
-    input_letters.append("input_letter")
+
+for j in range(int(number_input_letters)):
+    input_letter = input("v , c or l? ")
+    input_letters.append(str(input_letter))
+
+
 
 def generator():
-    for k in input_letters:
+    for i in range(int(number_input_letters)):
         if input_letters[i] == 'v':
-            input_letters[i] = random.choice(vowels)
+            letter1 = random.choice(vowels)
+            output_letters.append(letter1)
         elif input_letters[i] == 'c':
-            input_letters[i] == random.choice(consonants)
-        else: input_letters[i] == random.choice(string.ascii_letters)
-    name = input_letters 
-    print(name)
+            letter1 = random.choice(consonants)
+            output_letters.append(letter1)
+        else:
+            letter1 = random.choice(string.ascii_letters)
+            output_letters.append(letter1)
+        i = i + 1
+    name = output_letters
+    print(''.join(name))
+
+
+generator()
 
 
 
