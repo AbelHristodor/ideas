b7f939367ae5e5409d4e1ef540a6a61e4bec1037
Abel Hristodor <habelor@gmail.com>
2018-07-10 12:18:19 +0200

    Update Ps_scraper.py

--- Scraper_scripts/Ps_scraper.py
+++ Scraper_scripts/Ps_scraper.py
@@ -1,21 +1,16 @@
-'''
-Questo è uno semplice script che ricava i titoletti dal sito https://promessisposi.weebly.com
-dei capitoli dei promessi sposi. Il capitolo è a scelta dell'utente
-
-'''
 import requests
 from bs4 import BeautifulSoup
 
 try:
-    CAPITOLO = input("Di quale capitolo vuoi sapere i titoletti? Usa i numeri romani.\n")
+    CAPITOLO = input(
+        "Scegli un capitolo. Usa i numeri romani.\n")
 except:
     print("C'è stato un errore. Errore di scrittura?")
 URL = f"https://promessisposi.weebly.com/capitolo-{CAPITOLO}.html"
 RESP = requests.get(URL)
 SOUP = BeautifulSoup(RESP.text, 'lxml')
 
-# Selezioniamo i titoletti h2
-
+## ----------------------------TITOLETTI------------------------------------------
 TITOLI = SOUP.select("h2")
 
 print("\n\n\n\nEcco i titoletti:\n")
@@ -24,6 +19,7 @@ print("\n\n\n\nEcco i titoletti:\n")
 del TITOLI[-1]
 del TITOLI[0]
 del TITOLI[-1]
+del TITOLI[-1]
 
 C = 0
 # Si stampa ogni titoletto numerandolo.
@@ -31,3 +27,32 @@ for TITOLO in TITOLI:
     TEXT = TITOLO.get_text()
     C += 1
     print(str(C) + '.' + TEXT)
+
+
+
+
+##  ----------------------------Si passa ai Paragrafi-------------------------------
+PARAGRAFI = SOUP.select(".paragraph")
+
+dict = {}
+c = 0
+# Si prende il testo di ogni paragrafo
+for paragrafo in PARAGRAFI: 
+    testo = paragrafo.get_text()
+    dict.update({c:testo})
+    c +=1
+# Si elimina cò che non ci interessa
+for i in range(1,7):
+    del dict[c-i]
+del dict[0]
+del dict[1]
+del dict[2]
+
+
+
+
+## ----------------------------Interazione col programma-----------------------------
+def scegliParagrafo(n_titolo):
+    a = int(input("Che Paragrafo vuoi leggere? Usa i numeri."))
+    
+
